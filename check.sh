63584bb2dc692102f5c024b5a84bfe70bbcb6528
Tom Bigham <75551001+y2ktibltd@users.noreply.github.com>
2023-03-01 09:33:25 -0500

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
-count=$(ls "$1"/*"$1".csv | wc -l)
-if [ $count -lt 3 ]
+count=$( aws s3 ls s3://app-id-104385-dep-id-104386-uu-id-883uvj59jojc/intraday/"$1"/ | wc -l )
+if [ ! $count == 40 ]
 then
-	echo "$((3-$count)) Files missing"
-	aws s3 cp s3://sdlkjfghklfjghskdfhg/intraday/$1/$1.tar.gz .
-	echo "rm $1/*.csv"
-	echo "tar -xcvf $1.tar.gz"
+        if [ $count -gt 40 ]
+        then
+                echo "Interval has files for other times"
+                aws s3 ls s3://app-id-104385-dep-id-104386-uu-id-883uvj59jojc/intraday/"$1"/ | grep -L $1
+        else
+                echo "$((40-$count)) Files missing"
+                if [ ! -d $1 ]
+                then
+                        mkdir $1
+                fi
+                aws s3 cp s3://app-id-104385-dep-id-104386-uu-id-883uvj59jojc/intraday/"$1"/intraday-15-$1.tar.gz $1
+                tar -xvf $1/intraday-15-$1.tar.gz -C $1/
+        fi
 else
-	echo "All files found"
-	echo "tar -cvf *.csv $1.tara.gz"
+        echo "All 40 files found"
 fi
